016ec762133464a2ee8943f05b9d8a2e62e73437
rupeshmishra90 <rupeshtikapur@gmail.com>
2024-11-16 10:20:57 +0530

    create factory method to simplyfy test setup

--- MyEssentialFeedTests/MyEssentialFeedTests.swift
+++ MyEssentialFeedTests/MyEssentialFeedTests.swift
@@ -60,7 +60,7 @@ final class MyEssentialFeedTests: XCTestCase {
         let samples = [199,201,300,400,500]
         samples.enumerated().forEach{ index, code in
             expect(sut, toCompleteWith: .failure(.invalidData)) {
-                client.complete(withStatusCode: code, at: index)
+                client.complete(withStatusCode: code, data: <#Data#>, at: index)
             }
         }
         
@@ -149,7 +149,7 @@ final class MyEssentialFeedTests: XCTestCase {
         {
             messages[index].completion(.failure(error))
         }
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0)
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0)
         {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
             messages[index].completion(.success( data, response))
