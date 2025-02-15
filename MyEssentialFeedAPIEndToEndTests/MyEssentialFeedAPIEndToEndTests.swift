ed08d243c34589370837394f171144538ccc3c7f
rupeshmishra90 <rupeshtikapur@gmail.com>
2025-02-15 16:47:14 +0530

    Extract test setup to a helper method

--- MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
+++ MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
@@ -11,18 +11,8 @@ import MyEssentialFeed
 class MyEssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTextAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
-        let exp = expectation(description: "Wait for load completion")
-        var recievedResult: LoadFeedResult?
-        loader.load { result in
-            recievedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        switch recievedResult {
+        switch getFeedResult() {
             case let .success(items):
                 XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             items.enumerated().forEach { index, item in
@@ -36,7 +26,20 @@ class MyEssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
     // MARK: - Helpers
-    
+    func getFeedResult()-> LoadFeedResult?{
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        var recievedResult: LoadFeedResult?
+        loader.load { result in
+            recievedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        return recievedResult
+    }
     func expectedItem(at index: Int)-> FeedItem{
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at: index), imageURL: imageURL(at: index))
     }
