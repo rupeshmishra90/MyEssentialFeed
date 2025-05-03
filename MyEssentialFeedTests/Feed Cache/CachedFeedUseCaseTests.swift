bbc89d3f3127dba4cd286e406b9eea6d9ee7d55b
rupeshmishra90 <rupeshtikapur@gmail.com>
2025-05-03 21:54:10 +0530

    Remove redundent test code

--- MyEssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
+++ MyEssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
@@ -27,7 +27,6 @@ class LocalFeedLoader{
 class FeedStore{
     typealias DeletionCompletion = (Error?) -> Void
     var deleteCachedFeedCallCount = 0
-    var insertCallCount = 0
     var insertions = [(items: [FeedItem], timestamp: Date)]()
     private var deletionCompletions: [DeletionCompletion] = []
     func deleteCachedFeed(){
@@ -45,7 +44,6 @@ class FeedStore{
     }
     func insert(_ items: [FeedItem], timestamp: Date)
     {
-        insertCallCount += 1
         insertions.append((items, timestamp))
         
     }
@@ -71,16 +69,10 @@ class CachedFeedUseCaseTests: XCTestCase{
         let deletionError = anyNSError()
         sut.save(items)
         store.completeDeletion(with: deletionError)
-        XCTAssertEqual(store.insertCallCount, 0)
+        XCTAssertEqual(store.insertions.count, 0)
     }
     
-    func test_save_requestNewCacheInsertionOnSuccessfulDeletion(){
-        let items = [uniqueItem(), uniqueItem()]
-        let (sut, store) = makeSUT()
-        sut.save(items)
-        store.completeDeletionSuccessfully()
-        XCTAssertEqual(store.insertCallCount, 1)
-    }
+    
     
     func test_save_requestNewCacheInsertionWithTimestampOnSuccessfulDeletion(){
         let timestamp = Date()
